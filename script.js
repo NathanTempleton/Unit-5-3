90075c75726cfadd91098248159c0397464b4c97
NathanTempleton <nathan.templeton@ocsbstudent.ca>
2021-03-17 13:19:02 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//This will get the event listener to activate when the button with id report result is clicked, which will acivate the repeatText function.
+document.getElementById('repeat').addEventListener('click', repeatText)
+
+//This is a function that will create an alert for the value of the input.
+function repeatText() {
+  alert(document.getElementById('input').value)
+}
